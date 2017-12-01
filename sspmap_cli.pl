05466659cce51506422f9ee2c294e00fbb22600a
Anthony <anthony.carapetis@gmail.com>
2017-12-01 01:59:31 +0000

    added scaling support

--- sspmap_cli.pl
+++ sspmap_cli.pl
@@ -4,6 +4,14 @@ use strict;
 use warnings;
 
 use Image::Magick;
+use Getopt::Long;
+
+my $scale = 1;
+GetOptions ('scale=i' => \$scale);
+
+if ($scale < 1 or $scale > 5) {
+    die "Scale $scale not supported: must be in [1,5]";
+}
 
 my @COLORS = map {[ map { $_ / 256 } @$_ ]} (
     [10,137,244],    # trampoline / dark blue
@@ -37,6 +45,11 @@ while ($data =~ /\cF.(\d+),(\d+),(\d+),-?\d+,-?\d+/g) {
         next;
     }
     $img->SetPixel(x => $2, y => $3, color => $COLORS[$1]);
+}
+
+if ($scale > 1) {
+    my $size = 160 * $scale;
+    $img->Resize(width => $size, height => $size, filter => 'Point');
 }
 
 $img->Write('png:-');
